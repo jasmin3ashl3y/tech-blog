f97a6648a388dfdd04f42f51dc6c10a9622662b9
jasmin3ashl3y <jasmine@ashleyit.com>
2021-12-23 22:10:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
